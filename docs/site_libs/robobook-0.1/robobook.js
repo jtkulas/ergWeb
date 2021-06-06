4a9c2ae7971b5268232c225c77f91b333ad5410e
jtkulas <kulasj@montclair.edu>
2021-06-06 11:04:44 -0500

    cleaning up

--- docs/site_libs/robobook-0.1/robobook.js
+++ docs/site_libs/robobook-0.1/robobook.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 
+
     // Fix for dots in level 1 and level 2 titles
     $('body .section.level1').each(function () {
       $(this).attr("id", $(this).attr("id").replace(/\./g, "-"));
@@ -11,27 +12,22 @@ $( document ).ready(function() {
       $(this).attr("href", $(this).attr("href").replace(/\./g, "-"));
     });
 
+
+    if ($(document).width() <= 1000) {
+      $(".book").removeClass("with-summary");
+    }
     // Shift nav in mobile when clicking the menu.
-    $(document).on('click', "[data-toggle='wy-nav-top']", function() {
-      $("[data-toggle='wy-nav-shift']").toggleClass("shift");
-    });
-    // Close menu when you click a link.
-    $(document).on('click', "#toc ul li a", function() {
-      $("[data-toggle='wy-nav-shift']").removeClass("shift");
-    });
-    // Close menu when you click on main content
-    $(document).on('click', "#main, #header", function() {
-      $("[data-toggle='wy-nav-shift']").removeClass("shift");
+    $(document).on('click', ".toggle-sidebar", function() {
+      $(".book").toggleClass("with-summary");
     });
 
     // Make tables responsive
-    $("#main table").wrap("<div class='table-wrapper'></div>");
+    $(".book-body table").wrap("<div class='table-wrapper'></div>");
 
     // ScrollSpy also requires that we use a Bootstrap nav component.
     $('#toc ul').first().addClass('nav');
-    $('body').scrollspy({target: '#toc'});
+    $('.book-body').scrollspy({target: '#toc'});
 
-    // add sticky table headers
-    //$('table').stickyTableHeaders();
+    $("body").removeClass("preload");
 
 });
